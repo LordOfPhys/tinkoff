18aef9c73a70797b054e6a6d0a63453d7598095b
Anton <anton@MacBook-Air-Anton.local>
2019-05-24 09:45:18 +0300

    123

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,12 +3,27 @@ $(function() {
         if (document.getElementById('calendar').classList.contains('open')) {
             $('#calendar').toggleClass('open');
         }
+        if (document.getElementById('analys').classList.contains('open')) {
+            $('#analys').toggleClass('open');
+        }
         $('#documents').toggleClass('open');
     });
     $('#button_cal').on('click', function(){
         if (document.getElementById('documents').classList.contains('open')) {
             $('#documents').toggleClass('open');
         }
+        if (document.getElementById('analys').classList.contains('open')) {
+            $('#analys').toggleClass('open');
+        }
         $('#calendar').toggleClass('open');
+    });
+    $('#button_analys').on('click', function(){
+        if (document.getElementById('documents').classList.contains('open')) {
+            $('#documents').toggleClass('open');
+        }
+        if (document.getElementById('calendar').classList.contains('open')) {
+            $('#calendar').toggleClass('open');
+        }
+        $('#analys').toggleClass('open');
     });
 });
